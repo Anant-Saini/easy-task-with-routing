6d803afbe0f5b564b395ed8a1e1c3d3c25358f05
Anant Saini <anantsaini.india@gmail.com>
2026-05-23 23:50:57 +0530

    feat: lazy load tasks service

--- src/app/tasks/tasks.routes.ts
+++ src/app/tasks/tasks.routes.ts
@@ -1,25 +1,32 @@
 import { Routes } from '@angular/router';
 import { canDeactivateAddTask, NewTaskComponent } from './new-task/new-task.component';
 import { TasksComponent, tasksResolver } from './tasks.component';
+import { TasksService } from './tasks.service';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
-  },
-  {
-    path: 'tasks', //<domain>/users/:userId/tasks
-    component: TasksComponent,
-    runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: tasksResolver
-    }
-  },
-  {
-    path: 'tasks/new', // <domain>/users/:userId/tasks/new
-    component: NewTaskComponent,
-    title: 'Add Task',
-    canDeactivate: [canDeactivateAddTask]
+    providers: [TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', //<domain>/users/:userId/tasks
+        component: TasksComponent,
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: tasksResolver,
+        },
+      },
+      {
+        path: 'tasks/new', // <domain>/users/:userId/tasks/new
+        component: NewTaskComponent,
+        title: 'Add Task',
+        canDeactivate: [canDeactivateAddTask],
+      },
+    ],
   },
 ];
